06cf02867b0d7b0d037e26a285b2f245ff691f93
phlaster <phlaster@users.noreply.github.com>
2024-09-02 19:31:20 +0300

    enhanced custom Exception

--- ConfigException.py
+++ ConfigException.py
@@ -1,13 +1,22 @@
 class ConfigException(Exception):
     def __init__(self, *args):
-        if args:
+        if len(args) == 2:
+            self.line_no = int(args[0])
+            self.message = args[1]
+        elif len(args) == 1:
+            self.line_no = None
             self.message = args[0]
-        else:
+        elif len(args) == 0:
+            self.line_no = None
             self.message = None
+        else:
+            raise ValueError("Invalid number of arguments")
 
     def __str__(self):
+        if self.line_no:
+            return f"ConfigException at line {self.line_no}: {self.message}"
         if self.message:
-            return "ConfigException : {}".format(self.message)
+            return f"ConfigException : {self.message}"
         else:
             return "ConfigException"
 
