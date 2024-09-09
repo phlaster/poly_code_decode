3ba2290c45b3066274488a4f4df6efb5a0bc1ff1
phlaster <phlaster@users.noreply.github.com>
2024-09-09 12:16:26 +0300

    whitespace removal

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ class MainClass:
     def run(self, config_file_name: str) -> str:
         result = ""
         try:
-            configuration = self._config_reader.read_config(config_file_name)
-            file_name = configuration[self._config_reader.file_name_for_coder_param_name]
-            buffer_size = configuration[self._config_reader.buffer_size_param_name]
+            configuration = self._config_reader.read_config(config_file_name)            
+            file_name = configuration[self._config_reader.file_name_for_coder_param_name]            
+            buffer_size = configuration[self._config_reader.buffer_size_param_name]            
             coder_configuration = configuration[self._config_reader.coder_run_option_param_name]
 
             with self._file_reader.read_file(file_name, buffer_size) as chunks:
