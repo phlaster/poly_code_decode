463d60c3f18bd7b0246622286fd7545cbd601b47
phlaster <phlaster@users.noreply.github.com>
2024-09-09 12:17:14 +0300

    implemented proper buffering in read_file

--- MyFileReader.py
+++ MyFileReader.py
@@ -2,14 +2,13 @@ from FileReaderInterface import FileReaderInterface
 from contextlib import contextmanager
 
 class MyFileReader(FileReaderInterface):
+    
     @contextmanager
     def read_file(self, file_name: str, buffer_size: int):
-        file = None
         try:
-            file = open(file_name, 'r')
-            yield file
+            with open(file_name, 'r') as file:
+                yield iter(lambda: file.read(buffer_size), '')
         except IOError as e:
-            print(f"Error reading file: {e}")
-        finally:
-            if file:
-                file.close()
+            print(f"Error while reading file: {e}")
+        except Exception as e:
+            print(f"An error occurred while reading the file: {e}")
