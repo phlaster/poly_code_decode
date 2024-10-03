7bf3d986c932b83a6305d3d9dd0f3f301539fe77
phlaster <phlaster@users.noreply.github.com>
2024-10-03 21:41:23 +0300

    updated `MainClass.run` method: - printing result in a stream, instead of appending each chunk to a result string - added catching an unspecified Exception

--- main.py
+++ main.py
@@ -15,25 +15,26 @@ class MainClass:
         self._file_reader = MyFileReader()
         self._coder = MyCoder()
 
-    def run(self, config_file_name: str) -> str:
-        result = ""
+    def run(self, config_file_name: str):
         try:
             configuration = self._config_reader.read_config(config_file_name)            
             file_name = configuration[self._config_reader.file_name_for_coder_param_name]            
             buffer_size = configuration[self._config_reader.buffer_size_param_name]            
             coder_configuration = configuration[self._config_reader.coder_run_option_param_name]
-
+            
             with self._file_reader.read_file(file_name, buffer_size) as chunks:
                 for chunk in chunks:
-                    chunk_res = self._coder.run(coder_configuration, chunk)
-                    result += chunk_res
-                return result
+                    chunk_string = self._coder.run(coder_configuration, chunk)
+                    print(chunk_string, end='')
+        
         except ConfigException as e:
             logging.error(e)
-            return ""
+        except Exception as e:
+            logging.error(e)
+
 
 
 if __name__ == '__main__':
     argv_config_file_name = input()
     main_class = MainClass()
-    print(main_class.run(argv_config_file_name))
+    main_class.run(argv_config_file_name)
